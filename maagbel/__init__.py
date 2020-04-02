fd992d4845a4a013fb3e4930cc28ee63376b06a6
Torsten Sachse <torsten.sachse@gmail.com>
2020-04-02 12:37:51 +0200

    Correct wrong license information
    Was GPLv3 but needs to be GPLv2

--- maagbel/__init__.py
+++ maagbel/__init__.py
@@ -1,4 +1,28 @@
+"""A reduced version of the Open Babel chemical toolbox to work with
+ManipulateAggregates
+"""
+
+# Copyright (C) 2020 by Torsten Sachse
+# All rights reserved.
+#
+# This file is part of MaAg-bel.
+# The contents of this file are covered by the terms of the GPL v2 license,
+# see the file COPYING in the root if the repository for the license.
+#
+# MaAg-bel is free software: you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation version 2 of the License.
+#
+# MaAg-bel is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+# GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License
+# along with MaAg-bel. If not, see <http://www.gnu.org/licenses/>.
+
 import os
+
 # Set some environment variables needed by the library if not yet set
 __MAABEL_PATH = os.path.dirname(os.path.realpath(__file__))
 # The path to some data files needed by the forcefields
